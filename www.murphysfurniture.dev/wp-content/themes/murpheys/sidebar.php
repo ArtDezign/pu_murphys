028c0ce3cab0428b0573721afe9b89c8b340de8e
kano6940 <kano6940@pacificu.edu>
2017-03-01 14:05:02 -0800

    Improving the Sidebar
    Sidebar looks like christmas

--- www.murphysfurniture.dev/wp-content/themes/murpheys/sidebar.php
+++ www.murphysfurniture.dev/wp-content/themes/murpheys/sidebar.php
@@ -1,23 +1,19 @@
-<div class="container sidebar">
-    
-      <div class="row">
-        <div class="col-3 header-image">
+<div class="col-3" id="sidebar">
+    <div class="row" id="header-image">
+        
           	<img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width;?>" alt="murphy's Logo" />
-        </div>
+        
       </div>
       
-      <div class="row">
-    	<div class="col-3 site-identitiy">
+    	<div class="site-identity">
             <h1><?php echo get_bloginfo('name'); ?></h1>
             <h3><?php echo get_bloginfo('description'); ?></h3>
         </div>
-      </div>
-        
-      <div class="row">
-          <div class="col-3 navbar">
-          	<?php wp_nav_menu( array('menu' => 'Header Menu')); ?>
-          </div>
-      </div>
-    
-</div>
+      
+      <div class="row" id="menu">
           
+          	<?php wp_nav_menu( array('menu' => 'Header Menu')); ?>
+          
+      </div>
+</div>
+
